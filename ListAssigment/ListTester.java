683153c4c21a06913f6d0ccb9f005a7a3b5818f8
Karla Santibáñez <96139227+kbsg01@users.noreply.github.com>
2022-11-09 22:13:07 -0300

    Add files via upload

---
+++ ListAssigment/ListTester.java
@@ -0,0 +1,15 @@
+public class ListTester {
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.add(3);
+        sll.add(4);
+        sll.add(10);
+        sll.add(5);
+        sll.add(15);
+        sll.add(2);
+        sll.find(4);
+        sll.removeAt(5);
+        sll.remove();
+        sll.prinValues();
+    }
+}
